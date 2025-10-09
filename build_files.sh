638288a575f4b10f09563dbcc9e253ae8da5811f
go-arnold <arnoldciku@gmail.com>
2025-10-09 23:20:28 +0200

    solve

--- build_files.sh
+++ build_files.sh
@@ -7,8 +7,8 @@ python3.12 -m pip install -r requirements.txt
 export PATH="/python312/bin:$PATH"
 
 
-echo "Collect static files"
-python3.12 manage.py collectstatic 
+#echo "Collect static files"
+#python3.12 manage.py collectstatic 
 
 echo "Check media files"
 ls -l $MEDIA_ROOT

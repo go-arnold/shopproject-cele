f59f3ebe122bc1caba7355f34903d3f17b25103d
go-arnold <arnoldciku@gmail.com>
2025-10-09 23:12:19 +0200

    commit for vercel

---
+++ build_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Build the project
+echo "Building the project"
+python3.12 -m pip install -r requirements.txt
+
+export PATH="/python312/bin:$PATH"
+
+
+echo "Collect static files"
+python3.12 manage.py collectstatic 
+
+echo "Check media files"
+ls -l $MEDIA_ROOT

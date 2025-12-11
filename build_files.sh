4e66df00255914609fe7f5ff8b4209947f0fa07d
go-arnold <arnoldciku@gmail.com>
2025-12-11 22:53:29 +0200

    check_verc

--- build_files.sh
+++ build_files.sh
@@ -7,7 +7,4 @@ python3.12 -m pip install -r requirements.txt
 export PATH="/python312/bin:$PATH"
 
 #echo "Collect static files"
-#python3.12 manage.py collectstatic 
-
-echo "Check media files"
-ls -l $MEDIA_ROOT
+python3.12 manage.py collectstatic --noinput 

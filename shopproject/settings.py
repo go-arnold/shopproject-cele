a2b5188ec574a0b9033ded57dc081924136787af
go-arnold <arnoldciku@gmail.com>
2026-01-27 08:04:58 +0200

    updated to hosted database

--- shopproject/settings.py
+++ shopproject/settings.py
@@ -114,18 +114,18 @@ WSGI_APPLICATION = "shopproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "matabardb",
-        "USER": "matabar",
-        "PASSWORD": "LeNouveauPassword1@AllAccounts",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "matabardb",
+#         "USER": "matabar",
+#         "PASSWORD": "LeNouveauPassword1@AllAccounts",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("dbname"),
@@ -135,7 +135,7 @@ DATABASES = {
         'PORT': os.getenv("port"),
     }
 
-}"""
+}
 
 
 # Password validation
